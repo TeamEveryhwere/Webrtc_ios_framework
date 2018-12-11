3dfc9bd29c313996443f2f2077b2d07473e4d011
TeamEveryhwere <45547394+TeamEveryhwere@users.noreply.github.com>
2018-12-11 22:28:59 +0900

    VIdeoModule Edit

---
+++ IOS_FrameworkTests/Varistar_IOS_FrameworkTests.m
@@ -0,0 +1,37 @@
+//
+//  Varistar_IOS_FrameworkTests.m
+//  Varistar_IOS_FrameworkTests
+//
+//  Created by 이다한 on 2018. 11. 9..
+//  Copyright © 2018년 TeamEverywhere. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Varistar_IOS_FrameworkTests : XCTestCase
+
+@end
+
+@implementation Varistar_IOS_FrameworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
